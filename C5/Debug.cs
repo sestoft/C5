742f36c98b7a1f0e776e3e5e771da468cfda316c
Rasmus Nielsen <rasmus@dehler-nielsen.com>
2011-04-26 10:33:21 +0200

    Debug.cs added back

---
+++ C5/Debug.cs
@@ -0,0 +1,13 @@
+﻿namespace C5
+{
+    /// <summary>
+    /// Class containing debugging symbols - to eliminate preprocessor directives
+    /// </summary>
+    internal class Debug
+    {
+        /// <summary>
+        /// Flag used to test hashing. Set to true when unit testing hash functions.
+        /// </summary>
+        internal static bool UseDeterministicHashing { get; set; }
+    }
+}
