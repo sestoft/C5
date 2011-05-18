5071514b5dc6dbb45eaf33a9ed1ca5ff84a82e9a
Rasmus Nielsen <rasmus@dehler-nielsen.com>
2011-05-18 13:27:56 +0200

    Merged

---
+++ C5.UserGuideExamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// This file is part of the C5 Generic Collection Library for C# and CLI
+// See https://github.com/sestoft/C5/blob/master/LICENSE.txt for licensing details.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("C5.UserGuideExamples")]
+[assembly: AssemblyDescription("User guide examples for C5")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IT University of Copenhagen")]
+[assembly: AssemblyProduct("C5")]
+[assembly: AssemblyCopyright("Copyright © Niels Kokholm, Peter Sestoft, and Rasmus Nielsen 2003-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9cc9b3be-ff81-4007-b27f-a4a75a91aa3a")]
