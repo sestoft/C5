74711dff941138751c742c633e3dabf84efcb75b
Rasmus Nielsen <rasmus@dehler-nielsen.com>
2011-05-18 14:02:50 +0200

    License header

--- C5/Debug.cs
+++ C5/Debug.cs
@@ -1,4 +1,7 @@
-﻿namespace C5
+﻿// This file is part of the C5 Generic Collection Library for C# and CLI
+// See https://github.com/sestoft/C5/blob/master/LICENSE.txt for licensing details.
+
+namespace C5
 {
     /// <summary>
     /// Class containing debugging symbols - to eliminate preprocessor directives
