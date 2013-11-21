0b1917ce01a3f4eef0ac482631ac151d5f1ef0d9
Rasmus Nielsen <rasmus.nielsen@me.com>
2013-11-21 14:39:43 +0100

    Tweaked nuget packaging to support latest PCL

--- C5/package.bat
+++ C5/package.bat
@@ -1,7 +1,7 @@
 @echo off
 msbuild /t:Rebuild /p:Configuration=Release C5.csproj
-cmd /c echo F | xcopy /Y bin\Release\C5.dll "package\lib\portable-win+net40+sl40+wp+Xbox40\C5.dll"
-cmd /c echo F | xcopy /Y bin\Release\C5.xml "package\lib\portable-win+net40+sl40+wp+Xbox40\C5.xml"
+cmd /c echo F | xcopy /Y bin\Release\C5.dll "package\lib\portable-net4+sl5+wp8+win8\C5.dll"
+cmd /c echo F | xcopy /Y bin\Release\C5.xml "package\lib\portable-net4+sl5+wp8+win8\C5.xml"
 
 msbuild /t:Rebuild /p:Configuration=Release;PCL=false C5.csproj
 cmd /c echo F | xcopy /Y bin\Release\C5.dll "package\lib\net40\C5.dll"
