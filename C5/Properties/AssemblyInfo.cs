7e1595c088b576402e8eaa5035965883cc8e3d21
Rasmus Nielsen <rasmus.nielsen@me.com>
2014-06-10 15:41:01 +0200

    Updated copyright information and release notes

--- C5/Properties/AssemblyInfo.cs
+++ C5/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.*")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
