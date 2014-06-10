b2d9249a7ddb612b5614d6b19bbc4c211d9a42c6
Rasmus Nielsen <rasmus.nielsen@me.com>
2014-06-10 15:30:48 +0200

    Added test for serialization of ArrayList

---
+++ C5.NET40.Tests/ArrayListTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using NUnit.Framework;
+
+namespace C5.Tests
+{
+    [TestFixture]
+    public class ArrayListTests
+    {
+        [Test]
+        public void Serialize()
+        {
+            var source = new ArrayList<int>();
+            source.AddAll(new[] { 1, 2, 3, 4, 5 });
+
+            ArrayList<int> target;
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, source);
+                stream.Position = 0;
+
+                target = (ArrayList<int>) formatter.Deserialize(stream);
+            }
+
+            CollectionAssert.AreEqual(source, target);
+        }
+    }
+}
--- C5.NET40.Tests/Properties/AssemblyInfo.cs
+++ C5.NET40.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("C5.NET40.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit testing suite for C5.NET40")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("C5.NET40.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Niels Kokholm, Peter Sestoft, Rasmus Nielsen")]
+[assembly: AssemblyProduct("C5")]
+[assembly: AssemblyCopyright("Copyright © Niels Kokholm, Peter Sestoft, and Rasmus Nielsen 2003-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.*")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
