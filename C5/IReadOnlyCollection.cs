0c101585f2b238c9211c1078c7ee142333bca1de
Andrew Arnott <andrewarnott@gmail.com>
2017-07-02 17:28:33 -0700

    Consolidate NET35 build into .NET SDK project

--- C5/IReadOnlyCollection.cs
+++ C5/IReadOnlyCollection.cs
@@ -8,7 +8,17 @@ namespace C5
     /// Enables System.Collections.Generic.IReadOnlyCollection to be used in .NET 4.5 projects
     /// </summary>
     /// <typeparam name="T"></typeparam>
+#if NET35
+    public interface IReadOnlyCollection<T> : IEnumerable<T>, IEnumerable
+#else
     public interface IReadOnlyCollection<out T> : IEnumerable<T>, IEnumerable
+#endif
     {
+#if NET35
+        /// <summary>
+        /// Gets the number of elements in the collection.
+        /// </summary>
+        int Count { get; }
+#endif
     }
 }
--- C5/IReadOnlyList.cs
+++ C5/IReadOnlyList.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 namespace C5
 {
     /// <summary>
-    /// Represents a read-only collection of elements that can be accessed by index. 
+    /// Represents a read-only collection of elements that can be accessed by index.
     /// Enables System.Collections.Generic.IReadOnlyList to be used in .NET 4.5 projects
     /// </summary>
     /// <typeparam name="T"></typeparam>
+#if NET35
+    public interface IReadOnlyList<T> : IReadOnlyCollection<T>, IEnumerable<T>, IEnumerable
+#else
     public interface IReadOnlyList<out T> : IReadOnlyCollection<T>, IEnumerable<T>, IEnumerable
+#endif
     {
         /// <summary>
         /// Gets the element at the specified index in the read-only list.
