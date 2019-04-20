21b48c844d136a60da0a29b3bc0603621466571c
Rasmus Lystrøm <rasmusl@microsoft.com>
2019-04-20 09:14:39 +0200

    More compiling

--- C5.UserGuideExamples/GraphCopy.cs
+++ C5.UserGuideExamples/GraphCopy.cs
@@ -19,50 +19,55 @@ namespace C5.UserGuideExamples
         public static void Main(string[] args)
         {
             if (args.Length != 1)
+            {
                 Console.WriteLine("Usage: Graphcopy <nodecount>\n");
+            }
             else
             {
-                int count = int.Parse(args[0]);
-                Node<string>
-                  d = new Node<string>("d"),
-                  e = new Node<string>("e"),
-                  c = new Node<string>("c", d, e),
-                  b = new Node<string>("b", d),
-                  a = new Node<string>("a", d, b, c);
+                var count = int.Parse(args[0]);
+                var d = new Node<string>("d");
+                var e = new Node<string>("e");
+                var c = new Node<string>("c", d, e);
+                var b = new Node<string>("b", d);
+                var a = new Node<string>("a", d, b, c);
                 a[1] = a;
-                Node<string> newstart = CopyGraph1(a);
-                Console.WriteLine("Copy has same structure: " + Isomorphic1(newstart, a));
-                Console.WriteLine("newstart = " + newstart);
-                foreach (Node<string> node1 in newstart.children)
+                var newstart = CopyGraph1(a);
+                Console.WriteLine($"Copy has same structure: {Isomorphic1(newstart, a)}");
+                Console.WriteLine($"newstart = {newstart}");
+                foreach (var node1 in newstart.children)
+                {
                     Console.WriteLine(node1);
-                Node<string> node = new Node<string>("last");
-                for (int i = 0; i < count; i++)
+                }
+
+                var node = new Node<string>("last");
+                for (var i = 0; i < count; i++)
+                {
                     node = new Node<string>(i.ToString(), node);
+                }
+
                 newstart = CopyGraph1(node);
-                Console.WriteLine("Copy has same structure: "
-                          + Isomorphic1(newstart, node));
+                Console.WriteLine($"Copy has same structure: {Isomorphic1(newstart, node)}");
             }
         }
 
         // Graph copying 0
-
         public static Node<T> CopyGraph0<T>(Node<T> start)
         {
-            IDictionary<Node<T>, Node<T>> iso
-              = new HashDictionary<Node<T>, Node<T>>
-                    (EqualityComparer<Node<T>>.Default);
+            var iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default);
+
             return CopyNode0(iso, start);
         }
 
-        private static Node<T> CopyNode0<T>(IDictionary<Node<T>, Node<T>> iso,
-                                            Node<T> old)
+        private static Node<T> CopyNode0<T>(IDictionary<Node<T>, Node<T>> iso, Node<T> old)
         {
             if (!iso.Find(ref old, out var copy))
             {
                 copy = new Node<T>(old);
                 iso[old] = copy;
-                for (int i = 0; i < copy.children.Length; i++)
+                for (var i = 0; i < copy.children.Length; i++)
+                {
                     copy.children[i] = CopyNode0(iso, old.children[i]);
+                }
             }
             return copy;
         }
@@ -70,10 +75,9 @@ namespace C5.UserGuideExamples
         // Graph copying 1, using an explicit stack of pending nodes.
         // Every new node is added to the stack exactly once.  Every node
         // in the stack is also in the key set of the dictionary.
-
         public static Node<T> CopyGraph1<T>(Node<T> start)
         {
-            IDictionary<Node<T>, Node<T>> iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default);
+            var iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default);
             IStack<Node<T>> work = new ArrayList<Node<T>>();
             iso[start] = new Node<T>(start);
             work.Push(start);
@@ -96,10 +100,9 @@ namespace C5.UserGuideExamples
         }
 
         // Graph equality 0
-
         public static bool Isomorphic0<T>(Node<T> start1, Node<T> start2)
         {
-            IDictionary<Node<T>, Node<T>> iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default);
+            var iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default);
 
             return NodeEquals0(iso, start1, start2);
         }
@@ -134,10 +137,9 @@ namespace C5.UserGuideExamples
         // are the roots of isomorphic subgraphs.  The work stack contains
         // the assumptions needed to prove this.  Whenever node is on the
         // work stack, then node is also in the key set of iso.  
-
         public static bool Isomorphic1<T>(Node<T> start1, Node<T> start2)
         {
-            IDictionary<Node<T>, Node<T>> iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default)
+            var iso = new HashDictionary<Node<T>, Node<T>>(EqualityComparer<Node<T>>.Default)
             {
                 [start1] = start2
             };
--- C5.UserGuideExamples/GuardedView.cs
+++ C5.UserGuideExamples/GuardedView.cs
@@ -3,31 +3,34 @@
 
 // C5 example: 2006-06-26
 
-// Compile with 
-//   csc /r:C5.dll Anagrams.cs 
+// Compile and run with 
+//  dotnet clean
+//  dotnet build ../C5/C5.csproj
+//  dotnet build -p:StartupObject=C5.UserGuideExamples.GuardedView
+//  dotnet run
 
 using System;
-using C5;
-using SCG = System.Collections.Generic;
 
-namespace MyTest {
-  class MyTest {
-    public static void Main(String[] args) {
-      IList<int> list1 = new ArrayList<int>();
-      list1.AddAll(new int[] { 2, 5, 7, 11, 37 });
-      IList<int> list2 = new GuardedList<int>(list1);
-      IList<int> 
-        gv = new GuardedList<int>(list1.View(1,2)),
-        vg = list2.View(1,2);
-      IList<int> 
-        gvu = gv.Underlying, 
-        vgu = vg.Underlying;
-      Console.WriteLine(gvu);  // Legal 
-      Console.WriteLine(vgu);  // Legal 
-      // gv.Slide(+1);         // Illegal: guarded view cannot be slid
-      vg.Slide(+1);            // Legal: view of guarded can be slid
-      // gvu[1] = 9;           // Illegal: list is guarded
-      // vgu[1] = 9;           // Illegal: list is guarded
+namespace C5.UserGuideExamples
+{
+    class GuardedView
+    {
+        public static void Main(string[] args)
+        {
+            IList<int> list1 = new ArrayList<int> { 2, 5, 7, 11, 37 };
+            IList<int> gv = new GuardedList<int>(list1.View(1, 2));
+            IList<int> gvu = gv.Underlying;
+
+            IList<int> list2 = new GuardedList<int>(list1);
+            IList<int> vg = list2.View(1, 2);
+            IList<int> vgu = vg.Underlying;
+
+            Console.WriteLine(gvu);  // Legal 
+            Console.WriteLine(vgu);  // Legal 
+                                     // gv.Slide(+1);         // Illegal: guarded view cannot be slid
+            vg.Slide(+1);            // Legal: view of guarded can be slid
+                                     // gvu[1] = 9;           // Illegal: list is guarded
+                                     // vgu[1] = 9;           // Illegal: list is guarded
+        }
     }
-  }
 }
