e6222c24a2517bb6ba98957243440c459039772a
Rasmus Lystrøm <rasmus@lystroem.dk>
2024-06-06 20:10:18 +0200

    Fix null warnings

--- C5/Comparers/ComparerZeroHashCodeEqualityComparer.cs
+++ C5/Comparers/ComparerZeroHashCodeEqualityComparer.cs
@@ -12,29 +12,34 @@ namespace C5;
 /// <para><b>Note: this will give a new EqualityComparer each time created!</b></para>
 /// </summary>
 /// <typeparam name="T"></typeparam>
-internal class ComparerZeroHashCodeEqualityComparer<T> : SCG.IEqualityComparer<T>
+/// <remarks>
+/// Create a trivial <see cref="T:System.Collections.Generic.IEqualityComparer`1"/> compatible with the
+/// <see cref="T:System.Collections.Generic.IComparer`1"/> <code>comparer</code>
+/// </remarks>
+/// <param name="comparer"></param>
+internal class ComparerZeroHashCodeEqualityComparer<T>(SCG.IComparer<T> comparer) : SCG.IEqualityComparer<T>
 {
-    private readonly SCG.IComparer<T> comparer;
-    /// <summary>
-    /// Create a trivial <see cref="T:System.Collections.Generic.IEqualityComparer`1"/> compatible with the
-    /// <see cref="T:System.Collections.Generic.IComparer`1"/> <code>comparer</code>
-    /// </summary>
-    /// <param name="comparer"></param>
-    public ComparerZeroHashCodeEqualityComparer(SCG.IComparer<T> comparer)
-    {
-        this.comparer = comparer ?? throw new NullReferenceException("Comparer cannot be null");
-    }
+    private readonly SCG.IComparer<T> comparer = comparer;
+
     /// <summary>
     /// A trivial, inefficient hash function. Compatible with any equality relation.
     /// </summary>
     /// <param name="item"></param>
     /// <returns>0</returns>
-    public int GetHashCode(T item) { return 0; }
+    public int GetHashCode(T item) => 0;
+
     /// <summary>
     /// Equality of two items as defined by the comparer.
     /// </summary>
     /// <param name="item1"></param>
     /// <param name="item2"></param>
     /// <returns></returns>
-    public bool Equals(T item1, T item2) { return comparer.Compare(item1, item2) == 0; }
+    public bool Equals(T? item1, T? item2)
+    {
+        if (item1 is null && item2 is null) return true;
+        if (item1 is null) return false;
+        if (item2 is null) return false;
+
+        return comparer.Compare(item1, item2) == 0;
+    }
 }
